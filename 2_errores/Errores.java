59096311bb17962b28494d92e1cea417603d23dc
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-19 13:59:58 -0600

    Add files via upload

---
+++ 2_errores/Errores.java
@@ -0,0 +1,22 @@
+public class Errores {
+//el estandar de java pide que las clases comiencen con letra mayuscula
+
+  public static void main (String args []) {
+  System.out.println ("Soy Dafne");
+  }
+
+}
+
+/*
+ERRORES LOGICOS
+puede haber dos situaciones, error logico del programa o error logico del programador
+
+los errores logicos del programador suelen ser bastante sencillos como no seleccionar correctamente el archivo o la ubicacion
+de este
+
+los errores logicos del programa son los mas complicados de resolver ya que pueden ser miles de lineas
+
+Un error sintactico hace referencia a la "ortografia" del programa, falta algun simbolo, palabra mal escrita, etc
+no se permite la ejecucion de programas con errores sintaticos
+
+*/
