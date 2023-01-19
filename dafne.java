f4c2c6ee4a0fa493be59df54aa0b02dd36a83c33
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-19 13:20:53 -0600

    Add files via upload

---
+++ dafne.java
@@ -0,0 +1,21 @@
+public class dafne { //todo el programa estara dentro de una clase, incluso el main
+
+//comentario lineal
+
+/* Comentario multi linea*/
+
+	//Siempre es necesario el main
+	public static void main (String args[]) {
+	System.out.println("Tunas verdes");
+	//todas las instrucciones terminan en ;
+	}
+}
+
+/*
+	en terminal seleccionaremos nuestro arcihvo como "javac <nombre de la clase>.java",
+	una vez realizado eso, java nos creara un nuevo archivo clase, baicots(?
+	para que java lo entienda y pueda ajecutarlo
+
+	Para ejecutarlo en terminal y ver su funcionamiento bastara con poner
+	"java <nombre de la clase>"
+*/
