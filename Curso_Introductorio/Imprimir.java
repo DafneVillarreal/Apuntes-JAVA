15c192ba066af38eb5ec8f7875ce1afa721c4998
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-22 16:10:22 -0600

    Add files via upload

---
+++ Curso_Introductorio/Imprimir.java
@@ -0,0 +1,21 @@
+package imprimir;
+
+public class Imprimir {
+
+    public static void main(String[] args) {
+        System.out.print ("Prueba de impresion.");
+        /*
+        *System es una libreria del lenguaje
+        *escribimos "out" para ver los recursos que tenemos para la salida de informacion
+        *para la accion de print tenemos muchas opciones
+        */
+        
+        System.out.println ("salto, salto");
+        System.out.println(" ");
+        /*
+        *Entre los diferentes tipos de print, encontramos 'print' y 'println'
+        *La diferencia entre ellos es que println crea un salto de linea
+        */
+    }
+    
+}
