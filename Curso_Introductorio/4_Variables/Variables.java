328859b532a98107012b7d5b8a0689267e0ff18b
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-22 16:48:40 -0600

    Create Variables.java

---
+++ Curso_Introductorio/4_Variables/Variables.java
@@ -0,0 +1,25 @@
+package basico;
+
+/*
+*El curso se manejara con paquetes y archivos java class,
+*dentro de cada paquete estaran todos los ficheros necesarios
+*siempre que estos se encuentren dentro de un paquete sera neceesario hacer
+*referencia a este con la sintaxis
+    package <nombre del paquete>;
+*/
+
+public class Variables {
+ 
+/*
+*Toda clase o metodo es necesario ponerlo entre llaves {}, para asi encapsular 
+*la informacion y saber que corresponden a un escenario-
+    
+*Siempre sera necesario tener una clase principal, llamada main, definida con la
+*estructura
+    public static void main (String [] args) {}
+*/
+    
+    public static void main (String[] args) {
+        System.out.println ("Prueba");
+    }
+}
