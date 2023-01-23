33b0351b75ec3286d49f7a87056acd6281cd1bca
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-22 19:14:09 -0600

    Update Variables.java

--- Curso_Introductorio/4_Variables/Variables.java
+++ Curso_Introductorio/4_Variables/Variables.java
@@ -1,3 +1,34 @@
+/*
+Datos primitivos
+
+|TIPO    |   BITS    |   MINIMO                  |   MAXIMO               |
+|        |           |                           |                        |
+|byte    |   8       |   -128                    |   127                  |
+|short   |   16      |   -32768                  |   32767                |
+|int     |   32      |   -2147483648             |   2147483647           |
+|long    |   64      |   -9223372036854775808    |   9223372036854775807  |
+|float   |   32      |   -3.402823e38            |   3.402823e38          |
+|double  |   64      |   -1.79769313486232e308   |   1.79769313486232e308 |
+|chart   |   16      |   '\u000'                 |   '\ufff'              |
+
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Datos tipo objeto 
+
+* De las bibliotecas estandar de JAVA:
+    Sting, Scanner, ArrayList, etc
+
+* Definidos por el programador:
+    Cliente, Estudiante, Carro, etc
+
+*Array:
+    Vectores o matrices
+
+* Envoltorio o wrapper:
+    Byte, Short, Integer, Long, Float, Double, Character, Boolean
+
+*/
+
 package Basico;
 
 /*
@@ -21,5 +52,22 @@ public class Variables {
     
     public static void main (String[] args) {
         System.out.println ("Prueba");
+        
+        //JAVA es sensible a las mayusculas y minusculas
+        
+        int Edad;
+        String Nombre;
+        
+        Edad = 15;
+        Nombre = "Dafne";
+        double Sueldo = 1500.23;
+        Boolean Soltero = false; //or true
+        //asignamos mediante '=', cuando es texto lo pondremos entre comillas
+        
+        System.out.println (Edad);
+        System.out.println (Nombre);
+        System.out.println (Sueldo);
+        System.out.println (Soltero);
+        //Para imprimir basta con mandar la variable
     }
 }
