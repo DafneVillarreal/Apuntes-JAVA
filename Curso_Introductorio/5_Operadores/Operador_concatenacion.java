2821379e243e00a5553991dfbb6f51c0debb5721
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-22 21:15:54 -0600

    Add files via upload

---
+++ Curso_Introductorio/5_Operadores/Operador_concatenacion.java
@@ -0,0 +1,14 @@
+package Basico;
+
+public class Operador_concatenacion {
+    public static void main (String[] args) {
+        String Nombre = "El Tunas", Profesion = "Cerrajero";
+        
+        System.out.print ("Mi nombre es " +Nombre +"y mi profesion es ser " +Profesion);
+    }
+    
+    /*
+    El operador de concatenacion ( + ) nos permite concatenar cadenas de texto
+    y variables en la impresion de datos en pantalla 
+    */
+}
