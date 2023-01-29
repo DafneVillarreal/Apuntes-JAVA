da853e2458a2a406d410bb9c780c9ef5336178de
Dafne Villarreal <83681690+DafneVillarreal@users.noreply.github.com>
2023-01-29 17:08:41 -0600

    Add files via upload

---
+++ Curso_Introductorio/10_ciclos/Do_while.java
@@ -0,0 +1,12 @@
+package Basico;
+
+public class Do_while {
+    public static void main (String [] args) {
+        int Contador =1;
+        
+        do {
+            System.out.println (Contador);
+            Contador++;
+        } while (Contador <= 10);
+    }
+}
